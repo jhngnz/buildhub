4e8e9131b1bbf08946290c0079eb1440f438273b
jhngnz <142912206+jhngnz@users.noreply.github.com>
2025-09-30 16:59:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Construction Scheduling Platform</title>
-  <link rel="stylesheet" href="assets/css/style.css">
+  
 </head>
 <body>
-  <h1>🏗 Construction Scheduling Platform</h1>
-  <p>Connecting buyers & suppliers for construction materials</p>
-  <a href="login.php">Login</a> | <a href="register.php">Register</a>
+
 </body>
 </html>
